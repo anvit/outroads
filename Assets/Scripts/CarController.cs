e1c78e3882a56ba00e16718b1e0f5498aea40f26
Anvit Srivastav <anvit1@hotmail.com>
2019-11-24 12:50:51 -0800

    add player controller script

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public float maxSpeed = 3.0f;
+    public float currentSpeed = 0.0f;
+    private Vector3 change;
+    private bool isJump = false;
+    private Rigidbody rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        currentSpeed = currentSpeed + Mathf.Max((Input.GetAxisRaw("Vertical")), maxSpeed);
+        change.z = currentSpeed;
+        isJump = Input.GetButton("Jump");
+        rigidBody.MovePosition(transform.position + change * Time.deltaTime);
     }
 }
